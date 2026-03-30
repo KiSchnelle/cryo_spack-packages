c4478133d5c4eb50be60a6a17059a0c379f8d2d8
Kilian Schnelle <kischnelle@icloud.com>
2026-03-30 13:08:38 +0200

    type

--- spack_pkgs/spack_repo/strubios/general/packages/py_opencv_python/package.py
+++ spack_pkgs/spack_repo/strubios/general/packages/py_opencv_python/package.py
@@ -27,4 +27,4 @@ class PyOpencvPython(PythonPackage):
     # depends_on("py-numpy@:2", type=("build", "run"), when="^python@:3.8")
     depends_on("py-pip", type="build")
     depends_on("py-scikit-build", type="build")
-    depends_on("py-numpy:", type=("build", "run"))
+    depends_on("py-numpy", type=("build", "run"))
