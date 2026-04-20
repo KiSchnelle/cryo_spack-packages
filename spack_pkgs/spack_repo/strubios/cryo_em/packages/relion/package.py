9f7db9eaf6fbf2c86417ec6c50f1dde8d9845a3b
Kilian Schnelle <kischnelle@icloud.com>
2026-04-20 15:49:51 +0200

    try smaller relion

--- spack_pkgs/spack_repo/strubios/cryo_em/packages/relion/package.py
+++ spack_pkgs/spack_repo/strubios/cryo_em/packages/relion/package.py
@@ -33,95 +33,47 @@ class Relion(CMakePackage, CudaPackage):
         sha256="7e0d56fd4068c99f943dc309ae533131d33870392b53a7c7aae7f65774f667be",
     )
     version(
-        "4.0.0",
-        sha256="0987e684e9d2dfd630f1ad26a6847493fe9fcd829ec251d8bc471d11701d51dd",
-    )
-
-    # 3.1.4 latest release in 3.1 branch
-    version(
         "3.1.4",
         sha256="3bf3449bd2d71dc85d2cdbd342e772f5faf793d8fb3cda6414547cf34c98f34c",
     )
     version(
-        "3.1.3",
-        sha256="e67277200b54d1814045cfe02c678a58d88eb8f988091573453c8568bfde90fc",
-    )
-    version(
-        "3.1.2",
-        sha256="dcdf6f214f79a03d29f0fed2de58054efa35a9d8401543bdc52bfb177987931f",
-    )
-    version(
-        "3.1.1",
-        sha256="63e9b77e1ba9ec239375020ad6ff631424d1a5803cba5c608c09fd44d20b1618",
-    )
-    version(
-        "3.1.0",
-        sha256="8a7e751fa6ebcdf9f36046499b3d88e170c4da86d5ff9ad1914b5f3d178867a8",
-    )
-
-    # 3.0.8 latest release in 3.0 branch
-    version(
         "3.0.8",
         sha256="18cdd58e3a612d32413eb37e473fe8fbf06262d2ed72e42da20356f459260973",
     )
-    version(
-        "3.0.7",
-        sha256="a6d37248fc4d0bfc18f4badb7986dc1b6d6849baa2128b0b4dade13cb6991a99",
-    )
-
-    # relion master contains development code
-    # contains 3.0 branch code
-    version("master")
 
     variant("gui", default=True, description="build the gui")
-    variant("cuda", default=True, description="enable compute on gpu")
     variant("double", default=True, description="double precision (cpu) code")
     variant("double-gpu", default=False, description="double precision gpu")
 
-    # these new values were added in relion 3
-    # do not seem to cause problems with < 3
-    variant("mklfft", default=False, description="Use MKL rather than FFTW for FFT")
-    variant(
-        "allow_ctf_in_sagd",
-        default=True,
-        description=(
-            "Allow CTF-modulation in SAGD, as specified in Claim 1 of patent US10,282,513B2"
-        ),
-        when="@3",
+    conflicts(
+        "+double-gpu", when="~cuda", msg="Double precision GPU code requires CUDA"
     )
-    variant("altcpu", default=False, description="Use CPU acceleration", when="~cuda")
 
-    variant(
-        "external_motioncor2",
-        default=False,
-        description="Have external motioncor2 available in addition to Relion builtin",
-    )
+    variant("own-fftw", default=True, description="Use bundled FFTW.")
+    variant("amd-fftw", default=True, when="+own-fftw", description="Use AMD's FFTW.")
+    variant("mklfft", default=False, description="Use MKL rather than FFTW for FFT")
+
+    conflicts("+mklfft", when="+own-fftw", msg="Cannot use MKL FFT with bundled FFTW")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("mpi")
     depends_on("cmake@3:", type="build")
     depends_on("binutils@2.32:", type="build")
-    depends_on("fftw precision=float,double", when="~mklfft")
 
+    depends_on("ctffind@4", type="run")
+    depends_on("ghostscript", type="run", when="@4:")
     # use the +xft variant so the interface is not so horrible looking
     depends_on("fltk+xft", when="+gui")
-
     depends_on("libtiff")
     depends_on("libpng", when="@4:")
+    depends_on("pbzip2", type="run", when="@4:")
+    depends_on("xz", type="run", when="@4:")
+    depends_on("zstd", type="run", when="@4:")
 
-    depends_on("cuda@11.6:11.8", when="@3.0.7:4.0.1 +cuda")
-    depends_on("cuda@12:", when="@5.0.0: +cuda")
-    depends_on("tbb", when="+altcpu")
+    depends_on("cuda@11:", when="+cuda")
+
     depends_on("mkl", when="+mklfft")
-    depends_on("ctffind@4.1:4", type="run", when="@5:")
-    depends_on("ctffind@:4", type="run", when="@:4.0.1")
-    depends_on("motioncor2", type="run", when="+external_motioncor2")
-
-    depends_on("ghostscript", type="run", when="@4:")
-    depends_on("pbzip2", type="run", when="@5:")
-    depends_on("xz", type="run", when="@5:")
-    depends_on("zstd", type="run", when="@5:")
 
     # CUDA-aware deps — propagate cuda_arch
     for _arch in CudaPackage.cuda_arch_values:
@@ -146,24 +98,14 @@ class Relion(CMakePackage, CudaPackage):
         when="@5.1.0: ~cuda",
     )
 
-    patch(
-        "0002-Simple-patch-to-fix-intel-mkl-linking.patch",
-        when="@:3.1.1 os=ubuntu18.04",
-    )
-    patch(
-        "https://github.com/3dem/relion/commit/2daa7447c1c871be062cce99109b6041955ec5e9.patch?full_index=1",
-        sha256="4995b0d4bc24a1ec99042a4b73e9db84918eb6f622dacb308b718146bfb6a5ea",
-        when="@4.0.0",
-    )
-
     def cmake_args(self):
         args = [
             "-DGUI=%s" % ("+gui" in self.spec),
             "-DDoublePrec_CPU=%s" % ("+double" in self.spec),
             "-DDoublePrec_GPU=%s" % ("+double-gpu" in self.spec),
-            "-DALLOW_CTF_IN_SAGD=%s" % ("+allow_ctf_in_sagd" in self.spec),
+            "-DFORCE_OWN_FFTW=%s" % ("+own-fftw" in self.spec),
+            "-DAMDFFTW=%s" % ("+amd-fftw" in self.spec),
             "-DMKLFFT=%s" % ("+mklfft" in self.spec),
-            "-DALTCPU=%s" % ("+altcpu" in self.spec),
         ]
         if self.spec.satisfies("+gui"):
             incs = [
@@ -180,12 +122,6 @@ class Relion(CMakePackage, CudaPackage):
             else:
                 args += ["-DCUDA=ON", "-DCudaTexture=ON", "-DCUDA_ARCH=%s" % (carch)]
 
-            if self.spec.satisfies("@5:"):
-                cuda_flags = " ".join(
-                    CudaPackage.cuda_flags(self.spec.variants["cuda_arch"].value)
-                )
-                args += [f"-DCUDARCH={cuda_flags}"]
-
         if self.spec.satisfies("@5: ~cuda"):
             # Relion 5 defaults to CUDA=ON so it has to be explicitly disabled.
             args.append("-DCUDA=OFF")
@@ -194,36 +130,7 @@ class Relion(CMakePackage, CudaPackage):
             args.append(f"-DPYTHON_EXE_PATH={self.spec['python'].command.path}")
             args.append("-DFETCH_WEIGHTS=OFF")
 
-        if self.spec.satisfies("@:3 +cuda ^cuda@11.6:"):
-            args.append("-DCUDA_NVCC_FLAGS=-DCUB_NS_QUALIFIER=::cub")
-
         return args
-
-    def patch(self):
-        # Remove flags not recognized by the NVIDIA compilers
-        if self.spec.satisfies("%nvhpc"):
-            filter_file("-std=c99", "-c99", "src/apps/CMakeLists.txt")
-
-        # set up some defaults
-        filter_file(
-            r"(#define DEFAULTQSUBLOCATION).*",
-            r'\1 "{0}"'.format(join_path(self.spec.prefix.bin, "relion_qsub.csh")),
-            join_path("src", "pipeline_jobs.h"),
-        )
-        filter_file(
-            r"(#define DEFAULTCTFFINDLOCATION).*",
-            r'\1 "{0}"'.format(join_path(self.spec["ctffind"].prefix.bin, "ctffind")),
-            join_path("src", "pipeline_jobs.h"),
-        )
-
-        if "+external_motioncor2" in self.spec:
-            filter_file(
-                r"(#define DEFAULTMOTIONCOR2LOCATION).*",
-                r'\1 "{0}"'.format(
-                    join_path(self.spec["motioncor2"].prefix.bin, "MotionCor2")
-                ),
-                join_path("src", "pipeline_jobs.h"),
-            )
 
     def setup_run_environment(self, env):
         env.set("RELION_CTFFIND_EXECUTABLE", self.spec["ctffind"].prefix.bin.ctffind)
